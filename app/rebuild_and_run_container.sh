939a6dea72665a0d180239c21077ed5ca38034a9
Stefanos Pleros <stefanos@mystenlabs.com>
2025-06-07 00:24:07 +0300

    Adds docker compose functionality

--- app/rebuild_and_run_container.sh
+++ app/rebuild_and_run_container.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
 
-# Build the Docker image
-docker build --no-cache --tag "nutrition-schedule-page" .
-
-# Stop the container if it's running
-docker stop nutrition-schedule 2>/dev/null || true
-
-# Remove the container if it exists
-docker rm nutrition-schedule 2>/dev/null || true
-
-# Run the new container
-docker run -d --name nutrition-schedule -p 3000:3000 nutrition-schedule-page
+# Rebuild and restart the container using docker-compose
+docker compose down && docker compose up --build -d
