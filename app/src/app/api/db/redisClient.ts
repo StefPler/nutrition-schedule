cf558028d1e0fdfc0d00a3023638215c105bf2fa
Stefanos Pleros <stefanos@mystenlabs.com>
2025-06-15 23:09:59 +0300

    Updates redis client hostname

--- app/src/app/api/db/redisClient.ts
+++ app/src/app/api/db/redisClient.ts
@@ -2,7 +2,7 @@ import { createClient } from "redis";
 
 // 172.17.0.2 -- use this for docker container deployment
 const client = createClient({
-  socket: { host: "192.168.1.12", autoSelectFamily: true, port: 6379 },
+  socket: { host: "192.168.1.2", autoSelectFamily: true, port: 6379 },
 });
 
 client.on("error", (err) => console.log("Redis Client Error", err));
